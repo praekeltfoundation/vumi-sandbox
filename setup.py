08635f97ac4af4aa4663f3e544c6e5f8da9db7a7
Simon Cross <hodgestar@gmail.com>
2015-03-04 20:47:53 +0200

    Add Sphinx doc skeleton.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vxsandbox",
-    version="0.0.1",
+    version="0.5.0a",
     url='http://github.com/praekelt/vumi-sandbox',
     license='BSD',
     description="A sandbox application worker for Vumi.",
