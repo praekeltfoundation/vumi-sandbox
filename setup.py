c0f88134cf18b50414435ab3536cde77e0d792af
Rudi Giesler <rudi@praekelt.com>
2015-08-05 09:10:59 +0200

    Post release version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vxsandbox",
-    version="0.5.2",
+    version="0.5.3a0",
     url='http://github.com/praekelt/vumi-sandbox',
     license='BSD',
     description="A sandbox application worker for Vumi.",
