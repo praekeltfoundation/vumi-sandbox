9fafca11dc91413c11fb946732a9287b31e1180a
Rudi Giesler <rudi@praekelt.org>
2020-04-29 10:19:15 +0200

    Release 0.6.2-alpha1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vxsandbox",
-    version="0.6.1",
+    version="0.6.2-alpha1",
     url='http://github.com/praekelt/vumi-sandbox',
     license='BSD',
     description="A sandbox application worker for Vumi.",
