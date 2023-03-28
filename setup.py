bb7a1f65761fa686b36733198642f0a3b955e2a2
Jeremy Thurgood <jeremy@praekelt.org>
2023-03-28 17:31:11 +0200

    Newer automat breaks on ancient Python

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
+        'automat<22.10.0',  # Twisted dep, newer versions don't work with ancient Python.
         'Twisted>=20.3.0,<21.0.0',
         'vumi>=0.6.19',
     ],
